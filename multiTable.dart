72c902795e86f5be141af17bd99a684148d263ee
xclusiveskate <abdullahiafolabi08@gmail.com>
2022-09-27 07:43:10 -0700

    new multi table

--- multiTable.dart
+++ multiTable.dart
@@ -4,7 +4,12 @@ void main() {
   //print multiplication table of a number from 1-12 based on the user input
   List myConst = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
   var input = int.parse(stdin.readLineSync()!);
+  print("     ");
   for (var i = 1; i < myConst.length; i++) {
     print(input * myConst[i]);
+  }
+    print("     ");
+  for (var element in myConst) {
+    print(element * input);
   }
 }
